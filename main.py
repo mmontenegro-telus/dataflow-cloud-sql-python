67b23c6bc0eaf13a0c75237095fbb1b27fed4411
mmontenegro-telus <mariaines.montenegro@telus.com>
2022-02-17 12:21:30 -0600

    feat: setup initial config for pushing image

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 from __future__ import division, print_function, absolute_import
 import os
+import time
 import logging
 import argparse
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
-
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions, GoogleCloudOptions, StandardOptions
 from transformations.db import ReadFromDBFn
 
-def read_file(filename):
-    with open(filename, 'r') as f:
-        content = f.read()
-        f.close()
-        return content
+logging.getLogger().setLevel(logging.INFO)
+logging.info("Building pipeline ...")
 
 def run():
-    # get the cmd args
-    args, pipeline_args = get_args()
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Load from CSV into CloudSQL')
+    parser.add_argument('--project',required=True, help='Specify Google Cloud project')
+    parser.add_argument('--region', required=True, help='Specify Google Cloud region')
+    parser.add_argument('--input_path', required=True, help='Specify Cloud Storage file path')
+    parser.add_argument('--staging_location', required=True, help='Specify Cloud Storage bucket for staging')
+    parser.add_argument('--temp_location', required=True, help='Specify Cloud Storage bucket for temp')
+    parser.add_argument('--runner', required=True, help='Specify Apache Beam Runner')
+    parser.add_argument('--service_account_email', required=True, help='Run job with service account credentials')
+    parser.add_argument('--network', required=True, help='Specify Google Network to run workers')
+    parser.add_argument('--subnetwork', required=True, help='Specify Google subnetwork to run workers')
+    #parser.add_argument('--db-url', required=True, dest='db_url')
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = args.service_account_file
+    opts = parser.parse_args()
+
+    # Setting up the Beam pipeline options
+    options = PipelineOptions()
+    options.view_as(GoogleCloudOptions).project = opts.project
+    options.view_as(GoogleCloudOptions).region = opts.region
+    options.view_as(GoogleCloudOptions).staging_location = opts.staging_location
+    options.view_as(GoogleCloudOptions).temp_location = opts.temp_location
+    options.view_as(GoogleCloudOptions).temp_location = opts.temp_location
+    options.view_as(GoogleCloudOptions).service_account_email = opts.service_account_email
+    options.view_as(GoogleCloudOptions).job_name = '{0}{1}'.format('hsm-pipeline-',time.time_ns())
+    options.view_as(StandardOptions).runner = opts.runner
+
+    # Static input and output
+    #input = 'gs://{0}/MariaTestCSV.csv'.format(opts.project)
+    input_path = opts.input_path
 
     # Create the pipeline
-    options = PipelineOptions(pipeline_args)
     options.view_as(SetupOptions).save_main_session = True
     with beam.Pipeline(options=options) as p:
-        query = read_file('./sql/query.sql')
-
-        (
+        rows = (
             p
             | "Initialize" >> beam.Create([{}])
-            | "Reading tables" >> beam.ParDo(ReadFromDBFn(
-                url=args.db_url,
-                query=query,
-            ))
-            | "Log tables" >> beam.Map(lambda item: logging.info(item))
+            | 'ReadFromGCS' >> beam.io.ReadFromText(input_path)
+            | 'SplitValues' >> beam.Map(lambda x:x.split(","))
         )
-
-def get_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--service-account-file', dest='service_account_file')
-    parser.add_argument('--db-url', dest='db_url')
-
-    return parser.parse_known_args()
 
 
 if __name__ == '__main__':
